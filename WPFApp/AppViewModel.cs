7ccdd223c9dbdace0a545c9563f147cc8a365329
Ed Andersen <mail@edandersen.com>
2022-02-01 08:03:19 +0000

    Initial commit

---
+++ WPFApp/AppViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFApp
+{
+    public class AppViewModel : INotifyPropertyChanged
+    {
+        public AppViewModel(int initialCounterValue)
+        {
+            _counter = initialCounterValue;
+        }
+
+        private int _counter;
+
+        public int Counter
+        {
+            get
+            {
+                return _counter;
+            }
+            set
+            {
+                _counter = value;
+                OnPropertyChanged("Counter");
+            }
+        }
+
+        public ICommand Increment => new RelayCommand((p) => { Counter++; });
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
